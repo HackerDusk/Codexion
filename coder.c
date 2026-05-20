96889f3a3a62a9a231b81d44132ac23288d28263
Selison Mandresy Nomena Fitahiana Randro <srandro@student.42antananarivo.mg>
2026-05-21 00:14:43 +0300

    feat: added some content

---
+++ coder.c
@@ -0,0 +1,9 @@
+# include "codexion.h"
+
+void    take_dongle(t_dongle *dongle, t_coder *coder)
+{
+    pthread_mutex_lock(&dongle->mutex);
+    if (!dongle->available && !(!dongle->cooldown_end))
+        pthead_cond_wait(&dongle->cond, &dongle->mutex);
+    pthread_mutex_unlock(&dongle->mutex);
+}
